2bfef4529028fdc1268ae63c3ab89419947553a1
Amichai Berger <amichaiberger14@gmail.com>
2021-01-17 17:21:42 +0200

    excersize

---
+++ week2/day1/excersize.js
@@ -0,0 +1,56 @@
+//mixup
+/*
+let str1 = 'mix';
+let str2 = 'pod';
+let str3 = str1.charAt(str1.length-1)+" "+str2.charAt(str1.length-1)
+let newWord=str2.substring(0,str2.length-2);+str3+str1.substring(0,str2.length-2)*/
+//calculater
+/*let first = parseInt(prompt('first number'));
+let second = parseInt(prompt('second number'));
+alert(`the sum is ${first+second} `); */
+//nemo
+let string1 = "I am finding Nemo !"
+str=string1.split(" ");
+if (string1.indexOf("Nemo")>-1)
+console.log("I found Nemo at "+str.indexOf("Nemo")+"!")
+else console.log("I cant find nemo")
+//guess xp ninja
+/*5 >= 1//true
+    0 === 1//false
+    4 <= 1//false
+    1 != 1//false
+    "A" > "B"//false
+    "B" < "C"//true
+    "a" > "A"//true
+    "b" < "A"//false
+    true === false//false
+    true != true//false
+    
+    let c;
+    let a = 34;
+    let b = 21;
+    a = 2;
+    a + b
+    console.log(a + b);//23
+    console.log(3 + 4 + '5');//75
+
+    let num = prompt('numbers sep by comma', "2, 4, 6");
+    let sum;
+    sum=parseInt(num.charAt(0))+parseInt(num.charAt(3))
+    alert(`sum ${sum} `); */
+    /*
+    let num = parseInt(prompt('number', 4));
+    let boom="boo";
+    var i;
+    for (i = 2; i < num; i++) {
+    boom+="o"
+    }
+    boom+="m"
+    if ((num%2==0) && (num%5==0)){
+    boom+="!"
+    boom.toUpperCase();}
+    else if (num%2==0){
+    boom+="!"}
+    else if (num%5==0){
+    boom.toUpperCase();}
+    alert(boom)*/
