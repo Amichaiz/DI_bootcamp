c07957b164110b135a41a261853c82e442bd6718
Amichai Berger <amichaiberger14@gmail.com>
2021-01-17 12:51:02 +0200

    js

---
+++ week2/day1/fsj.js
@@ -0,0 +1,38 @@
+/*console.log("Hello");
+let addressNumber, addressStreet, country, global_address;
+addressNumber=39;
+addressStreet="tzahal";
+country="IL";
+console.log(addressNumber+ " " +addressStreet +" "+country);
+global_address="I live in "+addressNumber+ " " +addressStreet +" "+country;
+console.log(global_address);
+let birth=1996,
+fyear=2030,
+calc=fyear-birth;
+console.log("I will be "+ calc+ " in " +fyear);
+//alert("Hello");
+let pets=['cat','dog','fish','rabbit','cow'];
+console.log(pets[1]);
+pets.push('horse');
+pets.splice(3, 1)
+console.log(pets);*/
+/*let age = prompt('How old are you?', 20);
+alert(`You are ${age} years old!`); // You are 20 years old!*/
+/*let isBoss = confirm("Are you the boss?");
+alert(isBoss); // true if OK is pressed*/
+
+
+// guesses // correct answer
+5 + "34"// 39 //534
+5 - "4"//1
+10 % 5//0
+5 % 10//5
+"Java" + "Script"// JavaScrips
+" " + " "//"  "  
+" " + 0  //" 0"
+true + true //true//2
+true + false //false//1
+false + true// false//1
+false - true//false//-1
+3 - 4//-1
+"Bob" - "bill"//NaN
