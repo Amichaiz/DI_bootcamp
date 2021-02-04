7b7fcd2cee93c72ac905f4013786f3066ee2a376
Amichai Berger <amichaiberger14@gmail.com>
2021-02-04 13:06:12 +0200

    daily

---
+++ week4/day5/daily.js
@@ -0,0 +1,8 @@
+function reverseArray (arr){
+    let arra = []
+    for (let i = arr.length-1; i >= 0; i--) {
+        arra.push(arr[i])      
+    }
+    return arra
+}
+console.log(reverseArray([1,2,3,4]))
