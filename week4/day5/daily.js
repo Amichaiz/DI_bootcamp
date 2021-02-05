c4d2718415e05b5a53d9d4ed502064d919ad02b7
Amichai Berger <amichaiberger14@gmail.com>
2021-02-05 10:18:52 +0200

    reviw

--- week4/day5/daily.js
+++ week4/day5/daily.js
@@ -1,12 +1,20 @@
 /*without creating a new array
 function reverseArray(arr){return arr.reverse();}
 */
-
+/*
 function reverseArray (arr){
     let arra = []
     for (let i = arr.length-1; i >= 0; i--) {
         arra.push(arr[i])      
     }
     return arra
+}*/
+
+function reverseArray (arr){
+    let orlength = arr.length 
+    for (let i = orlength-1; i >= 0; i--) {
+        arr.push(arr[i])      
+    }
+    return arr.slice(orlength)
 }
 console.log(reverseArray([1,2,3,4]))
