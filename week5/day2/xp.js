ccb4442e554af1515c71f0ad22092a05d365a10c
Amichai Berger <amichaiberger14@gmail.com>
2021-02-08 15:43:32 +0200

    xp gold

--- week5/day2/xp.js
+++ week5/day2/xp.js
@@ -1,8 +1,19 @@
+//xp
 let xhr = new XMLHttpRequest();
 xhr.open('GET', "https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=dc6zaTOxFJmzC");
 xhr.responseType = 'json'
 xhr.send()
-
 xhr.onload = function() {
-  console.log(xhr.response)
+  makeGif(xhr.response)
 };
+//xp gold
+function makeGif(e){
+  let rnd = Math.floor(Math.random()*50)
+  domgif(e.data[rnd].id)
+  console.log(e);
+}
+function domgif(id){
+let gif = document.createElement("img")
+gif.setAttribute("src",`https://media2.giphy.com/media/${id}/giphy.gif`)
+document.body.appendChild(gif);
+}
