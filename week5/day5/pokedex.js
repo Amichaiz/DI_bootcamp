10a52552b37694b9a83cbd66d59c7ccdcbb11cc5
Amichai Berger <amichaiberger14@gmail.com>
2021-02-12 12:06:51 +0200

    poke

--- week5/day5/pokedex.js
+++ week5/day5/pokedex.js
@@ -1,21 +1,42 @@
 const func = async function () {
+    spin.style.display = "block"
     try{
-    let rnd = Math.floor(Math.random() * 897+1)
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${rnd}`)
     const data = await response.json()
-    console.log(data)
+    spin.style.display = 'none'
     html(data)  
     }catch{
         document.getElementById("name").innerHTML = "problem with pokeman"
+        spin.style.display = 'none'
     }
 }
 function html(data) {
+    document.getElementById("img").setAttribute("src",data.sprites.front_default)
     document.getElementById("name").innerHTML = `${data.name}`
     document.getElementById("number").innerHTML = `Pokemon n° ${data.id}`
     document.getElementById("height").innerHTML = `Height: ${data.height}cm`
     document.getElementById("weight").innerHTML = `Weight: ${data.weight}gr`
-    document.getElementById("type").innerHTML = `Type ${data.types[0].type.name}`        
+    document.getElementById("type").innerHTML = `Type: ${data.types[0].type.name}`        
 }
-func()/*
+const random = async function () {
+    rnd = Math.floor(Math.random() * 897+1)
+    func()
+}
+const rndu = async function () {
+    rnd++
+    func()
+}
+const rndd = async function () {
+    rnd--
+    func()
+}
+let rnd = 0
+let spin = document.getElementById('spinner')
 let but = document.getElementById("but")
-but.addEventListener("click", func)*/
+but.addEventListener("click", random)
+
+let boxr = document.getElementById("boxr")
+boxr.addEventListener("click", rndu)
+
+let boxl = document.getElementById("boxl")
+boxl.addEventListener("click", rndd)
