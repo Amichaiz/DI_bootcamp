87748489f70e311a335fa62ef6838dfb2d79fcdf
Amichai Berger <amichaiberger14@gmail.com>
2021-02-15 01:05:02 +0200

    hi

--- week6/day1/xp gold.sql
+++ week6/day1/xp gold.sql
@@ -21,3 +21,21 @@ select fname, lname from students where fname like 'a%';
 select fname, lname from students where fname like '%a_';
 select fname, lname from students where id =3 or id = 1;
 select fname, lname, birth_date from students where birth_date>'1/01/2000';
+
+ninja
+
+select fname, lname, birth_date from students
+ORDER BY
+    lname asc 
+FETCH FIRST 4 ROW ONLY;
+
+SELECT min(birth_date) AS min_date
+FROM   students
+
+daily challenge
+
+select max(actor_id) from actor;
+2. Try to add a new actor, with some blank fields. What happens ?
+ERROR:  null value in column "last_name" of relation "actor" violates not-null constraint
+DETAIL:  Failing row contains (203, Marc, null, 1998-11-02 00:00:00).
+SQL state: 23502
