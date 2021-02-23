8b7a536fb5d35345e94b0d50041f9062fb63d3cb
Amichai Berger <amichaiberger14@gmail.com>
2021-02-23 14:22:09 +0200

    xp

---
+++ week7/day3/fsxp.js
@@ -0,0 +1,35 @@
+
+//exercise 1
+const fs = require("fs");
+fs.readFile('./xp.txt', (err,data) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(data.toString());
+    }
+})
+
+//exercise 2
+
+const data='writing to a file'
+fs.writeFile('./exer2.txt', data, err => {
+    if(err){
+        console.log(err);
+    }
+})
+
+//exercise3
+
+let str = 'more bla bla bla ';
+fs.appendFile('./exer2.txt', str, err => {
+    if(err){
+        console.log(err);
+    }
+})
+
+fs.unlink('./exer2.txt', err => {
+    if(err){
+        console.log(err);
+    }
+})
