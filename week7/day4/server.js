dc2b3b7d9e0d46f95d21e2fa69b04cc785f8e517
Amichai Berger <amichaiberger14@gmail.com>
2021-02-24 12:19:00 +0200

    xp

--- week7/day4/server.js
+++ week7/day4/server.js
@@ -5,39 +5,39 @@ const knex = require('knex');
 const app = exp();
 /*
 CREATE TABLE users (
-	id serial PRIMARY KEY,
-	username VARCHAR ( 50 ) UNIQUE NOT NULL,
-	created_on TIMESTAMP NOT NULL
+  id serial PRIMARY KEY,
+  username VARCHAR ( 50 ) UNIQUE NOT NULL,
+  created_on TIMESTAMP NOT NULL
 );*/
-app.use(bp.urlencoded({extended:false}));
+app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
 
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '12345',
-    database : 'main'
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: '12345',
+    database: 'main'
   }
 });
 
-app.use('/',exp.static(__dirname+'/public'));
+app.use('/', exp.static(__dirname + '/public'));
 
-app.post('/user',(req,res)=>{
+app.post('/user', (req, res) => {
   console.log(req.body);
   db('users').insert(
     {
       username: req.body.user
     }
   )
-  .returning('*')
-  .then(data => {
-    res.send({message:'OK'})
-  })
-  .catch(err =>{
-    res.send({message:err.detail})
-  })
+    .returning('*')
+    .then(data => {
+      res.send({ message: 'OK' })
+    })
+    .catch(err => {
+      res.send({ message: err.detail })
+    })
 })
 /*
 function createuser({user}){
@@ -49,16 +49,32 @@ function createuser({user}){
   .returning('*')
 }*/
 
-app.get('/show', (req,res)=>{
- db('users').select('username')
- .then(data =>{
-   res.send(data)
- })
- .catch(err =>{
-  res.send({message:err.detail})
+app.get('/show', (req, res) => {
+  db('users').select('username')
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
+      res.send({ message: err.detail })
+    })
 })
+app.post('/find', (req, res) => {
+  console.log(req.body);
+  const { user } = req.body;
+  db('users')
+    .select('id', 'username')
+    .where({ username: user })
+    .then(data => {
+      console.log(data);
+      if(data.length>0){
+        res.send({message:`Found=>${data[0].username} id=> ${data[0].id}`})
+      }
+      else res.send({message:'not found'})
+    })
+    .catch(err => {
+      res.send({ message: err.detail })
+    })
 })
-
 
 
 
