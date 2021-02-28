b4b0652bd72e39d8f92cc5f8fd1d76d3b1afc87a
Amichai Berger <amichaiberger14@gmail.com>
2021-02-28 17:59:35 +0200

    xp

---
+++ week8/day1/Hello.js
@@ -0,0 +1,15 @@
+import React from  'react';
+
+const Hello = () => {
+  
+    return (
+        <>
+        <div style={{border: " 2px solid grey"}}>
+            Hello World
+        </div>
+               
+        </>
+    )
+}
+
+export default Hello;
---
+++ week8/day1/Jsx.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Jsx = () => {
+  
+ /*   exercise 2
+ const my =<h1>I Love JSX!</h1>
+    return (my);
+
+     exercise 3
+     return <h1>I do not Love JSX!</h1>;
+
+     exercise 4 
+     const my =<h1>react is {5+5} times beeteer then jsx</h1>
+     return (my);
+
+     exercise 5 
+     const my =
+     <form>
+         <li>Apples</li>
+         <li>Banananas</li>
+         <li>Cherries</li>
+     </form>
+     
+     return (my);
+
+     exercise 6 
+     const my =<div>
+     <header>I am a header</header>
+     <header>Im a better header</header></div>
+     return (my);
+
+     exercise 7 
+     const my =<div>
+     <input type="text"></input></div>
+     return (my);
+
+     exercise 8 */
+     let name= 'ami'
+     let age= 12
+     const my =
+     <div>
+    {   
+    `${name} is ${age} years old`
+    }
+    </div>
+     return (my);
+}
+export default Jsx
