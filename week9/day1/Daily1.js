f0208edd27331e4d0d6b7ac7702961fe47f50c0b
Amichai Berger <amichaiberger14@gmail.com>
2021-03-07 17:53:33 +0200

    daily1

---
+++ week9/day1/Daily1.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import countries from './components/countries';
+import contries from './components/countries'
+
+class Daily1 extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: '',  
+            contriesleft: [],
+        }
+    }
+    handleChange = (e) => {
+        const value = e.target.value;
+        this.setState({ [e.target.name]: value })
+        console.log(e.target.value);
+        this.setState({contriesleft:countries.filter(word => word.toLocaleLowerCase().includes(e.target.value))})
+        console.log(this.state.contriesleft);
+    }
+    fill = (e) => {
+        document.getElementById('contry').value=e.target.innerHTML
+    }
+       
+    render() {
+        const {contriesleft}=this.state        
+        return (
+            <>
+            <input id='contry' type='text' name='title' onChange={this.handleChange} placeholder='Contries' />
+            {contriesleft.map(item=>{return(
+            <div>
+                <p onClick={this.fill}>{item}</p><br/>
+            </div>)
+        })}
+        <h5>{contriesleft.length}</h5>
+            </>
+        )
+    }
+}
+
+export default Daily1; 
